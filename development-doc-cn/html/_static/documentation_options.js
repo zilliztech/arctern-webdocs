fba2ec5891fde1894c1a010552a9cf653c6fbff3
Emma-Song <64460989+Emma-Song@users.noreply.github.com>
2020-06-03 16:43:38 +0800

    update (#34)

--- development-doc-cn/html/_static/documentation_options.js
+++ development-doc-cn/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.x',
+    VERSION: 'gis version : 0.1.1build type : Release/CPU build time : 2020-05-11 08:10.53commit id : ecd116a6af480a44b545964d1b7a99ed3beb2400',
     LANGUAGE: 'zh_CN',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
